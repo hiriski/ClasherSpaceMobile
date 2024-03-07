fa9153636cb9d6103bdbdf27205407b0cf220bdc
hiriski <hiriski@outlook.com>
2024-03-07 18:01:15 +0700

    feat: update react native 0.73.5

--- ios/ClasherSpaceTests/ClasherSpaceTests.m
+++ ios/ClasherSpaceTests/ClasherSpaceTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ClasherSpaceTests : XCTestCase
+@interface ClasherSpaceTest : XCTestCase
 
 @end
 
-@implementation ClasherSpaceTests
+@implementation ClasherSpaceTest
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
